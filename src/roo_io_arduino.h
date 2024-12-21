5713c72feb4b91b53e38999f61d15a618fb80279
dejwk <dawid.kurzyniec@gmail.com>
2024-12-21 01:45:00 +0100

    Made compliant with the Arduino registry.

---
+++ src/roo_io_arduino.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "roo_io_arduino/fs/arduino_sdfs.h"
+#include "roo_io_arduino/fs/arduino_file_iterable.h"
+#include "roo_io_arduino/fs/arduino_file_resource.h"
+#include "roo_io_arduino/fs/arduino_file_input_stream.h"
+#include "roo_io_arduino/fs/arduino_file_output_stream.h"
