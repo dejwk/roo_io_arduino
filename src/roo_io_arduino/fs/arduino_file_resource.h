c7996f728f34fadb214e3facd334984f8470a229
dejwk <dawid.kurzyniec@gmail.com>
2024-12-28 02:41:09 +0100

    Added an 'extended' file resource which can take either an Arduino FS or a roo_io::Filesystem.

--- src/roo_io_arduino/fs/arduino_file_resource.h
+++ src/roo_io_arduino/fs/arduino_file_resource.h
@@ -5,6 +5,7 @@
 #include <memory>
 
 #include "roo_io/core/resource.h"
+#include "roo_io/fs/filesystem.h"
 #include "roo_io_arduino/fs/arduino_file_input_stream.h"
 
 namespace roo_io {
@@ -22,6 +23,29 @@ class ArduinoFileResource : public MultipassResource {
  private:
   FS& fs_;
   String path_;
+};
+
+// Can act both as an Arduino file resource, or as a roo_io::Filesystem-based
+// file resource.
+class ExtendedArduinoFileResource : public roo_io::MultipassResource {
+ public:
+  ExtendedArduinoFileResource(fs::FS& fs, std::string path)
+      : fs_(nullptr), arduino_fs_(&fs), path_(std::move(path)) {}
+
+  ExtendedArduinoFileResource(roo_io::Filesystem& fs, std::string path)
+      : fs_(&fs), arduino_fs_(nullptr), path_(std::move(path)) {}
+
+  std::unique_ptr<roo_io::MultipassInputStream> open() const override {
+    return fs_ != nullptr ? fs_->mount().fopen(path_.c_str())
+                          : std::unique_ptr<roo_io::MultipassInputStream>(
+                                new roo_io::ArduinoFileInputStream(
+                                    arduino_fs_->open(path_.c_str())));
+  }
+
+ private:
+  ::fs::FS* arduino_fs_;
+  roo_io::Filesystem* fs_;
+  std::string path_;
 };
 
 }  // namespace roo_io
