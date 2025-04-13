493c9417c823fc8f5076a1bf7f96eadfbea8c5d4
dejwk <dawid.kurzyniec@gmail.com>
2025-04-13 17:49:43 +0200

    Making ReliableSerial implement Stream, making it a near drop-in-replacement for Serial.

--- src/roo_io_arduino/serial/reliable_serial.cpp
+++ src/roo_io_arduino/serial/reliable_serial.cpp
@@ -1,6 +1,6 @@
 #include "roo_io_arduino/serial/reliable_serial.h"
 
-#include <thread>
+#include "Arduino.h"
 
 namespace roo_io {
 
@@ -20,6 +20,52 @@ void ReliableSerial::loop() {
   processor_.sendLoop();
 }
 
+namespace {
+
+size_t TimedWrite(StreamingRetransmitter& target, const roo::byte* buf,
+                  size_t count, unsigned long timeout) {
+  unsigned long start = millis();
+  size_t total = 0;
+  while (count > 0) {
+    for (int i = 0; i < 1000; ++i) {
+      size_t result = target.tryWrite(buf, count);
+      if (result == 0) {
+        target.recvLoop();
+        target.sendLoop();
+      } else {
+        total += result;
+        count -= result;
+      }
+      if (millis() - start > timeout) break;
+      delay(1);
+    }
+    return total;
+  }
+}
+
+size_t TimedRead(StreamingRetransmitter& source, roo::byte* buf, size_t count,
+                 unsigned long timeout) {
+  unsigned long start = millis();
+  size_t total = 0;
+  while (count > 0) {
+    for (int i = 0; i < 1000; ++i) {
+      size_t result = source.tryRead(buf, count);
+      if (result == 0) {
+        source.recvLoop();
+        source.sendLoop();
+      } else {
+        total += result;
+        count -= result;
+      }
+    }
+    if (millis() - start > timeout) break;
+    delay(1);
+  }
+  return total;
+}
+
+}  // namespace
+
 size_t ReliableSerial::ReliableOutputStream::write(const roo::byte* buf,
                                                    size_t count) {
   if (status() != roo_io::kOk) return 0;
@@ -38,7 +84,7 @@ size_t ReliableSerial::ReliableOutputStream::write(const roo::byte* buf,
       processor_.recvLoop();
       processor_.sendLoop();
     }
-    std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    delay(1);
   }
 }
 
@@ -59,8 +105,41 @@ size_t ReliableSerial::ReliableInputStream::read(roo::byte* buf, size_t count) {
       processor_.recvLoop();
       processor_.sendLoop();
     }
-    std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    delay(1);
   }
 }
+
+int ReliableSerial::available() { return 0; }
+
+int ReliableSerial::read() {
+  roo::byte result;
+  size_t count = processor_.tryRead(&result, 1);
+  return count > 0 ? (int)result : -1;
+}
+
+int ReliableSerial::peek() { return processor_.peek(); }
+
+size_t ReliableSerial::readBytes(char* buffer, size_t length) {
+  return TimedRead(processor_, (roo::byte*)buffer, length, getTimeout());
+}
+
+size_t ReliableSerial::readBytes(uint8_t* buffer, size_t length) {
+  return TimedRead(processor_, buffer, length, getTimeout());
+}
+
+size_t ReliableSerial::write(uint8_t val) {
+  out().write((const roo::byte*)&val, 1);
+  return 1;
+}
+
+size_t ReliableSerial::write(const uint8_t* buffer, size_t size) {
+  return out().writeFully((const roo::byte*)buffer, size);
+}
+
+int ReliableSerial::availableForWrite() {
+  return processor_.availableForWrite();
+}
+
+void ReliableSerial::flush() { out().flush(); }
 
 }  // namespace roo_io
--- src/roo_io_arduino/serial/reliable_serial.h
+++ src/roo_io_arduino/serial/reliable_serial.h
@@ -9,15 +9,32 @@
 
 namespace roo_io {
 
-class ReliableSerial {
+class ReliableSerial : public Stream {
  public:
   ReliableSerial(decltype(Serial)& serial, unsigned int sendbuf_log2,
                  unsigned int recvbuf_log2);
 
+  // Obtains the input stream that can be used to read from the reliable serial.
   roo_io::InputStream& in() { return in_; }
+
+  // Obtains the output stream that can be used to write to the reliable serial.
   roo_io::OutputStream& out() { return out_; }
 
   void loop();
+
+  // Stream overrides.
+
+  int available() override;
+  int read() override;
+  int peek() override;
+
+  size_t readBytes(char* buffer, size_t length) override;
+  size_t readBytes(uint8_t* buffer, size_t length) override;
+
+  size_t write(uint8_t) override;
+  size_t write(const uint8_t* buffer, size_t size) override;
+  int availableForWrite() override;
+  void flush() override;
 
  private:
   class ReliableOutputStream : public roo_io::OutputStream {
