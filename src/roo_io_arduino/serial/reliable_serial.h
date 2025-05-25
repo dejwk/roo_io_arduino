d5bdd87d734af6eef2548055713c3320810451e4
dejwk <dawid.kurzyniec@gmail.com>
2025-05-25 23:36:21 +0200

    Moved input/output out to separate files in roo_io.

--- src/roo_io_arduino/serial/reliable_serial.h
+++ src/roo_io_arduino/serial/reliable_serial.h
@@ -4,6 +4,8 @@
 #include "roo_io/memory/load.h"
 #include "roo_io/memory/store.h"
 #include "roo_io/reliable/bidi_streaming/internal/thread_safe/channel.h"
+#include "roo_io/reliable/bidi_streaming/internal/thread_safe/channel_input.h"
+#include "roo_io/reliable/bidi_streaming/internal/thread_safe/channel_output.h"
 #include "roo_io/reliable/packet_transport/over_stream/packet_receiver_over_stream.h"
 #include "roo_io/reliable/packet_transport/over_stream/packet_sender_over_stream.h"
 #include "roo_io_arduino/stream/arduino_stream_input_stream.h"
@@ -42,58 +44,9 @@ class ReliableSerial {
     roo_io::OutputStream& out() { return out_; }
 
    private:
-    class ReliableOutputStream : public roo_io::OutputStream {
-     public:
-      ReliableOutputStream(Channel& channel, uint32_t my_stream_id)
-          : channel_(channel), my_stream_id_(my_stream_id), status_(kOk) {}
-
-      size_t write(const roo::byte* buf, size_t count) override;
-
-      size_t tryWrite(const roo::byte* buf, size_t count) override;
-
-      int availableForWrite();
-
-      void flush() override;
-
-      void close() override;
-
-      roo_io::Status status() const override { return status_; }
-
-     private:
-      Channel& channel_;
-      uint32_t my_stream_id_;
-      roo_io::Status status_;
-    };
-
-    class ReliableInputStream : public roo_io::InputStream {
-     public:
-      ReliableInputStream(Channel& channel, uint32_t my_stream_id)
-          : channel_(channel), my_stream_id_(my_stream_id), status_(kOk) {}
-
-      void close() override;
-
-      size_t read(roo::byte* buf, size_t count) override;
-
-      size_t tryRead(roo::byte* buf, size_t count) override;
-
-      int available();
-      int read();
-      int peek();
-
-      size_t timedRead(roo::byte* buf, size_t count,
-                       roo_time::Interval timeout);
-
-      roo_io::Status status() const override { return status_; }
-
-     private:
-      Channel& channel_;
-      uint32_t my_stream_id_;
-      roo_io::Status status_;
-    };
-
     Channel& channel_;
-    ReliableInputStream in_;
-    ReliableOutputStream out_;
+    ChannelInput in_;
+    ChannelOutput out_;
   };
 
   ReliableSerial(decltype(Serial)& serial, unsigned int sendbuf_log2,
