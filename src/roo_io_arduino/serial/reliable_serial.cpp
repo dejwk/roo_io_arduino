35591c143420d6be0849f91c203a6ef9facb5ed8
dejwk <dawid.kurzyniec@gmail.com>
2025-05-25 13:59:40 +0200

    Refactored and updated ReliableSerial.

--- src/roo_io_arduino/serial/reliable_serial.cpp
+++ src/roo_io_arduino/serial/reliable_serial.cpp
@@ -4,142 +4,186 @@
 
 namespace roo_io {
 
+ReliableSerial::Connection::Connection(Channel& channel, uint32_t my_stream_id)
+    : channel_(channel),
+      in_(channel_, my_stream_id),
+      out_(channel_, my_stream_id) {}
+
 ReliableSerial::ReliableSerial(decltype(Serial)& serial,
                                unsigned int sendbuf_log2,
-                               unsigned int recvbuf_log2)
+                               unsigned int recvbuf_log2, std::string label,
+                               ConnectionCb connection_cb)
     : output_(serial),
       input_(serial),
       sender_(output_),
       receiver_(input_),
-      processor_(sender_, receiver_, sendbuf_log2, recvbuf_log2),
-      in_(processor_),
-      out_(processor_) {}
-
-void ReliableSerial::loop() {
-  processor_.recvLoop();
-  processor_.sendLoop();
+      channel_(sender_, receiver_, sendbuf_log2, recvbuf_log2,
+               std::move(connection_cb)),
+      connection_(nullptr) {
+#ifdef ESP32
+  serial.onReceive([this]() { channel_.tryRecv(); });
+#endif
 }
 
-namespace {
+// void ReliableSerial::loop() { channel_.loop(); }
 
-size_t TimedWrite(StreamingRetransmitter& target, const roo::byte* buf,
-                  size_t count, unsigned long timeout) {
-  unsigned long start = millis();
+size_t ReliableSerial::Connection::ReliableOutputStream::write(
+    const roo::byte* buf, size_t count) {
+  if (status_ != kOk) return 0;
+  if (count == 0) return 0;
+  return channel_.write(buf, count, my_stream_id_, status_);
+  // while (true) {
+  //   for (int i = 0; i < 100; ++i) {
+  //     size_t result = processor_.tryWrite(buf, count);
+  //     if (result > 0) {
+  //       // if (count > result) {
+  //       //   buf += result;
+  //       //   count -= result;
+  //       //   result += processor_.tryWrite(buf, count);
+  //       // }
+  //       return result;
+  //     }
+  //     processor_.loop();
+  //   }
+  //   delay(1);
+  // }
+}
+
+size_t ReliableSerial::Connection::ReliableOutputStream::tryWrite(
+    const roo::byte* buf, size_t count) {
+  if (status_ != kOk) return 0;
+  return channel_.tryWrite(buf, count, my_stream_id_, status_);
+}
+
+int ReliableSerial::Connection::ReliableOutputStream::availableForWrite() {
+  if (status_ != kOk) return 0;
+  return channel_.availableForWrite(my_stream_id_, status_);
+}
+
+void ReliableSerial::Connection::ReliableOutputStream::flush() {
+  if (status_ != kOk) return;
+  channel_.flush(my_stream_id_, status_);
+}
+
+void ReliableSerial::Connection::ReliableOutputStream::close() {
+  if (status_ != kOk) return;
+  channel_.close(my_stream_id_, status_);
+  if (status_ == kOk) status_ = kClosed;
+}
+
+void ReliableSerial::Connection::ReliableInputStream::close() {
+  if (status_ != kOk && status_ != kEndOfStream) return;
+  channel_.closeInput(my_stream_id_, status_);
+  if (status_ != kOk && status_ != kEndOfStream) return;
+  status_ = kClosed;
+}
+
+size_t ReliableSerial::Connection::ReliableInputStream::read(roo::byte* buf,
+                                                             size_t count) {
+  if (count == 0 || status_ != kOk) return 0;
+  return channel_.read(buf, count, my_stream_id_, status_);
+  // // processor_.loop();
+  // while (true) {
+  //   for (int i = 0; i < 100; ++i) {
+  //     size_t result = processor_.tryRead(buf, count);
+  //     if (result > 0) {
+  //       // if (count > result) {
+  //       //   buf += result;
+  //       //   count -= result;
+  //       //   result += processor_.tryRead(buf, count);
+  //       // }
+  //       return result;
+  //     }
+  //     processor_.loop();
+  //   }
+  //   delay(1);
+  // }
+}
+
+size_t ReliableSerial::Connection::ReliableInputStream::tryRead(roo::byte* buf,
+                                                                size_t count) {
+  if (count == 0 || status_ != kOk) return 0;
+  return channel_.tryRead(buf, count, my_stream_id_, status_);
+}
+
+int ReliableSerial::Connection::ReliableInputStream::available() {
+  if (status_ != kOk) return 0;
+  return channel_.availableForRead(my_stream_id_, status_);
+}
+
+int ReliableSerial::Connection::ReliableInputStream::read() {
+  if (status_ != kOk) return 0;
+  roo::byte result;
+  size_t count = channel_.tryRead(&result, 1, my_stream_id_, status_);
+  if (count > 0) return (int)result;
+  return -1;
+}
+
+int ReliableSerial::Connection::ReliableInputStream::peek() {
+  if (status_ != kOk) return -1;
+  int result = channel_.peek(my_stream_id_, status_);
+  if (result >= 0) return result;
+  return -1;
+}
+
+size_t ReliableSerial::Connection::ReliableInputStream::timedRead(
+    roo::byte* buf, size_t count, roo_time::Interval timeout) {
+  roo_time::Uptime start = roo_time::Uptime::Now();
   size_t total = 0;
+  if (status_ != kOk) return -1;
   while (count > 0) {
-    for (int i = 0; i < 1000; ++i) {
-      size_t result = target.tryWrite(buf, count);
+    for (int i = 0; i < 100; ++i) {
+      size_t result = tryRead(buf, count);
       if (result == 0) {
-        target.recvLoop();
-        target.sendLoop();
+        if (status_ != kOk) return -1;
+        channel_.loop();
       } else {
         total += result;
         count -= result;
       }
-      if (millis() - start > timeout) break;
-      delay(1);
+      if (count == 0) return total;
     }
-    return total;
-  }
-}
-
-size_t TimedRead(StreamingRetransmitter& source, roo::byte* buf, size_t count,
-                 unsigned long timeout) {
-  unsigned long start = millis();
-  size_t total = 0;
-  while (count > 0) {
-    for (int i = 0; i < 1000; ++i) {
-      size_t result = source.tryRead(buf, count);
-      if (result == 0) {
-        source.recvLoop();
-        source.sendLoop();
-      } else {
-        total += result;
-        count -= result;
-      }
-    }
-    if (millis() - start > timeout) break;
+    if (roo_time::Uptime::Now() - start > timeout) break;
     delay(1);
   }
   return total;
 }
 
-}  // namespace
+int ReliableSerial::Connection::available() { return in_.available(); }
 
-size_t ReliableSerial::ReliableOutputStream::write(const roo::byte* buf,
-                                                   size_t count) {
-  if (status() != roo_io::kOk) return 0;
-  if (count == 0) return 0;
-  while (true) {
-    for (int i = 0; i < 1000; ++i) {
-      size_t result = processor_.tryWrite(buf, count);
-      if (result > 0) {
-        // if (count > result) {
-        //   buf += result;
-        //   count -= result;
-        //   result += processor_.tryWrite(buf, count);
-        // }
-        return result;
-      }
-      processor_.recvLoop();
-      processor_.sendLoop();
-    }
-    delay(1);
-  }
+int ReliableSerial::Connection::read() { return in_.read(); }
+
+int ReliableSerial::Connection::peek() { return in_.peek(); }
+
+size_t ReliableSerial::Connection::readBytes(char* buffer, size_t length) {
+  return in_.timedRead((roo::byte*)buffer, length,
+                       roo_time::Millis(getTimeout()));
 }
 
-size_t ReliableSerial::ReliableInputStream::read(roo::byte* buf, size_t count) {
-  if (status() != roo_io::kOk) return 0;
-  if (count == 0) return 0;
-  while (true) {
-    for (int i = 0; i < 1000; ++i) {
-      size_t result = processor_.tryRead(buf, count);
-      if (result > 0) {
-        // if (count > result) {
-        //   buf += result;
-        //   count -= result;
-        //   result += processor_.tryRead(buf, count);
-        // }
-        return result;
-      }
-      processor_.recvLoop();
-      processor_.sendLoop();
-    }
-    delay(1);
-  }
+size_t ReliableSerial::Connection::readBytes(uint8_t* buffer, size_t length) {
+  return in_.timedRead((roo::byte*)buffer, length,
+                       roo_time::Millis(getTimeout()));
 }
 
-int ReliableSerial::available() { return 0; }
-
-int ReliableSerial::read() {
-  roo::byte result;
-  size_t count = processor_.tryRead(&result, 1);
-  return count > 0 ? (int)result : -1;
-}
-
-int ReliableSerial::peek() { return processor_.peek(); }
-
-size_t ReliableSerial::readBytes(char* buffer, size_t length) {
-  return TimedRead(processor_, (roo::byte*)buffer, length, getTimeout());
-}
-
-size_t ReliableSerial::readBytes(uint8_t* buffer, size_t length) {
-  return TimedRead(processor_, buffer, length, getTimeout());
-}
-
-size_t ReliableSerial::write(uint8_t val) {
+size_t ReliableSerial::Connection::write(uint8_t val) {
   out().write((const roo::byte*)&val, 1);
   return 1;
 }
 
-size_t ReliableSerial::write(const uint8_t* buffer, size_t size) {
+size_t ReliableSerial::Connection::write(const uint8_t* buffer, size_t size) {
   return out().writeFully((const roo::byte*)buffer, size);
 }
 
-int ReliableSerial::availableForWrite() {
-  return processor_.availableForWrite();
+int ReliableSerial::Connection::availableForWrite() {
+  return out_.availableForWrite();
 }
 
-void ReliableSerial::flush() { out().flush(); }
+void ReliableSerial::Connection::flush() { out().flush(); }
+
+std::shared_ptr<ReliableSerial::Connection> ReliableSerial::connect() {
+  uint32_t my_stream_id = channel_.connect();
+  connection_.reset(new Connection(channel_, my_stream_id));
+  return connection_;
+}
 
 }  // namespace roo_io
--- src/roo_io_arduino/serial/reliable_serial.h
+++ src/roo_io_arduino/serial/reliable_serial.h
@@ -1,86 +1,129 @@
+#include <memory>
+
 #include "roo_io/core/output_stream.h"
 #include "roo_io/memory/load.h"
 #include "roo_io/memory/store.h"
-#include "roo_io/reliable/packet_receiver.h"
-#include "roo_io/reliable/packet_sender.h"
-#include "roo_io/reliable/streaming_retransmitter.h"
+#include "roo_io/reliable/bidi_streaming/internal/thread_safe/channel.h"
+#include "roo_io/reliable/packet_transport/over_stream/packet_receiver_over_stream.h"
+#include "roo_io/reliable/packet_transport/over_stream/packet_sender_over_stream.h"
 #include "roo_io_arduino/stream/arduino_stream_input_stream.h"
 #include "roo_io_arduino/stream/arduino_stream_output_stream.h"
 
 namespace roo_io {
 
-class ReliableSerial : public Stream {
+class ReliableSerial {
  public:
+  using ConnectionCb = Channel::ConnectionCb;
+
+  class Connection : public Stream {
+   public:
+    Connection(Channel& channel, uint32_t my_stream_id);
+
+    // Status status() const;
+
+    int available() override;
+    int read() override;
+    int peek() override;
+
+    size_t readBytes(char* buffer, size_t length) override;
+    size_t readBytes(uint8_t* buffer, size_t length) override;
+
+    size_t write(uint8_t) override;
+    size_t write(const uint8_t* buffer, size_t size) override;
+    int availableForWrite() override;
+    void flush() override;
+
+    // Obtains the input stream that can be used to read from the reliable
+    // serial.
+    roo_io::InputStream& in() { return in_; }
+
+    // Obtains the output stream that can be used to write to the reliable
+    // serial.
+    roo_io::OutputStream& out() { return out_; }
+
+   private:
+    class ReliableOutputStream : public roo_io::OutputStream {
+     public:
+      ReliableOutputStream(Channel& channel, uint32_t my_stream_id)
+          : channel_(channel), my_stream_id_(my_stream_id), status_(kOk) {}
+
+      size_t write(const roo::byte* buf, size_t count) override;
+
+      size_t tryWrite(const roo::byte* buf, size_t count) override;
+
+      int availableForWrite();
+
+      void flush() override;
+
+      void close() override;
+
+      roo_io::Status status() const override { return status_; }
+
+     private:
+      Channel& channel_;
+      uint32_t my_stream_id_;
+      roo_io::Status status_;
+    };
+
+    class ReliableInputStream : public roo_io::InputStream {
+     public:
+      ReliableInputStream(Channel& channel, uint32_t my_stream_id)
+          : channel_(channel), my_stream_id_(my_stream_id), status_(kOk) {}
+
+      void close() override;
+
+      size_t read(roo::byte* buf, size_t count) override;
+
+      size_t tryRead(roo::byte* buf, size_t count) override;
+
+      int available();
+      int read();
+      int peek();
+
+      size_t timedRead(roo::byte* buf, size_t count,
+                       roo_time::Interval timeout);
+
+      roo_io::Status status() const override { return status_; }
+
+     private:
+      Channel& channel_;
+      uint32_t my_stream_id_;
+      roo_io::Status status_;
+    };
+
+    Channel& channel_;
+    ReliableInputStream in_;
+    ReliableOutputStream out_;
+  };
+
   ReliableSerial(decltype(Serial)& serial, unsigned int sendbuf_log2,
-                 unsigned int recvbuf_log2);
+                 unsigned int recvbuf_log2, std::string label,
+                 ConnectionCb connection_cb = nullptr);
 
-  // Obtains the input stream that can be used to read from the reliable serial.
-  roo_io::InputStream& in() { return in_; }
+  void begin() { channel_.begin(); }
 
-  // Obtains the output stream that can be used to write to the reliable serial.
-  roo_io::OutputStream& out() { return out_; }
-
-  void loop();
+  // void loop();
 
   // Stream overrides.
 
-  int available() override;
-  int read() override;
-  int peek() override;
+  std::shared_ptr<Connection> connect();
 
-  size_t readBytes(char* buffer, size_t length) override;
-  size_t readBytes(uint8_t* buffer, size_t length) override;
+  uint32_t packets_sent() const { return channel_.packets_sent(); }
+  uint32_t packets_delivered() const { return channel_.packets_delivered(); }
+  uint32_t packets_received() const { return channel_.packets_received(); }
 
-  size_t write(uint8_t) override;
-  size_t write(const uint8_t* buffer, size_t size) override;
-  int availableForWrite() override;
-  void flush() override;
+  size_t receiver_bytes_received() const { return receiver_.bytes_received(); }
+
+  size_t receiver_bytes_accepted() const { return receiver_.bytes_accepted(); }
 
  private:
-  class ReliableOutputStream : public roo_io::OutputStream {
-   public:
-    ReliableOutputStream(StreamingRetransmitter& processor)
-        : processor_(processor) {}
-
-    size_t write(const roo::byte* buf, size_t count) override;
-
-    size_t tryWrite(const roo::byte* buf, size_t count) override {
-      return processor_.tryWrite(buf, count);
-    }
-
-    void flush() override { processor_.flush(); }
-
-    roo_io::Status status() const override { return roo_io::kOk; }
-
-   private:
-    StreamingRetransmitter& processor_;
-  };
-
-  class ReliableInputStream : public roo_io::InputStream {
-   public:
-    ReliableInputStream(StreamingRetransmitter& processor)
-        : processor_(processor) {}
-
-    size_t read(roo::byte* buf, size_t count) override;
-
-    size_t tryRead(roo::byte* buf, size_t count) override {
-      return processor_.tryRead(buf, count);
-    }
-
-    roo_io::Status status() const override { return roo_io::kOk; }
-
-   private:
-    StreamingRetransmitter& processor_;
-  };
-
   roo_io::ArduinoStreamOutputStream output_;
   roo_io::ArduinoStreamInputStream input_;
-  roo_io::PacketSender sender_;
-  roo_io::PacketReceiver receiver_;
+  roo_io::PacketSenderOverStream sender_;
+  roo_io::PacketReceiverOverStream receiver_;
 
-  StreamingRetransmitter processor_;
-  ReliableInputStream in_;
-  ReliableOutputStream out_;
+  Channel channel_;
+  std::shared_ptr<Connection> connection_;
 };
 
 }  // namespace roo_io
