7e98118a55dff6e25cea0e2908bfb4c3760829f0
dejwk <dawid.kurzyniec@gmail.com>
2026-03-08 15:10:31 +0100

    Updated deps.

--- src/roo_io_arduino.h
+++ src/roo_io_arduino.h
@@ -1,5 +1,9 @@
 #pragma once
 
+/// Umbrella header for the roo_io_arduino module.
+///
+/// Arduino-specific implementations of roo_io filesystem interfaces.
+
 #include "roo_io_arduino/fs/arduino_sdfs.h"
 #include "roo_io_arduino/fs/arduino_file_iterable.h"
 #include "roo_io_arduino/fs/arduino_file_resource.h"
